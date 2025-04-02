ba941878a6ad5a2b75b23b37879afd49b5516b9b
putter-kamolprabha <c27thinnaphat.kamolprabha@afacademy.af.edu>
2025-04-01 22:41:02 -0600

    another 2nd try

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -48,35 +48,32 @@
 --|
 --+----------------------------------------------------------------------------
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
-  
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+ 
 entity thunderbird_fsm_tb is
 end thunderbird_fsm_tb;
-
+ 
 architecture test_bench of thunderbird_fsm_tb is 
-	
 	component thunderbird_fsm is 
-	  port(
+	   port(
 	       i_clk, i_reset : in std_logic;
 	       i_left, i_right: in std_logic;
 	       o_lights_L     : out std_logic_vector(2 downto 0);
 	       o_lights_R     : out std_logic_vector(2 downto 0)
 	   );
-	end component thunderbird_fsm;
-
-	-- test I/O signals
+	end component;
+ 
+	-- Signals
 	signal w_R : std_logic := '0';
 	signal w_RL : std_logic_vector(1 downto 0) := "00";
 	signal w_clk : std_logic := '0';
 	signal w_lights : std_logic_vector(5 downto 0) := "000000";
-	
-	-- constants
+ 
 	constant k_clk_period : time := 10 ns;
-	
-	
+ 
 begin
-	-- PORT MAPS ----------------------------------------
+	-- Instantiate Unit Under Test (UUT)
 	uut: thunderbird_fsm port map (
 	   i_clk => w_clk,
 	   i_reset => w_R,
@@ -89,25 +86,20 @@ begin
 	   o_lights_R(1) => w_lights(4),
 	   o_lights_R(2) => w_lights(5)
 	);
-	
-	-----------------------------------------------------
-	
-	-- PROCESSES ----------------------------------------	
-    -- Clock process ------------------------------------
-    clk_proc : process
+ 
+	-- Clock generation
+	clk_proc : process
 	begin
 		w_clk <= '0';
         wait for k_clk_period/2;
 		w_clk <= '1';
 		wait for k_clk_period/2;
 	end process;
-    
-	-----------------------------------------------------
-	
-	-- Test Plan Process --------------------------------
+ 
+	-- Simulation process
 	sim_proc: process
-	begin
-	   w_RL <= "10";
+	begin		
+		w_RL <= "10";
 		wait for k_clk_period*1;
 		  assert w_lights = "000001" report "bad left 1" severity failure;
 		wait for k_clk_period*1;
@@ -116,30 +108,27 @@ begin
 		  assert w_lights = "000111" report "bad left 3" severity failure;
 		wait for k_clk_period*1;
 		  assert w_lights = "000000" report "bad left R" severity failure;
-		
-	   w_RL <= "00";
+ 
+		w_RL <= "00";
 		wait for k_clk_period*1;
 		  assert w_lights = "000000" report "bad stop" severity failure;
-		  
-	   w_RL <= "01";
-	    wait for k_clk_period*1;
+ 
+        w_RL <= "01";
+		wait for k_clk_period*1;
 		  assert w_lights = "001000" report "bad right 1" severity failure;
 		wait for k_clk_period*1;
 		  assert w_lights = "011000" report "bad right 2" severity failure;
 		wait for k_clk_period*1;
 		  assert w_lights = "111000" report "bad right 3" severity failure;
 		wait for k_clk_period*1;
-		  assert w_lights = "000000" report "bad right R" severity failure;  
-		  
-	   w_RL <= "11";
-	    wait for k_clk_period*1;
+		  assert w_lights = "000000" report "bad right R" severity failure;
+ 
+		w_RL <= "11";
+		wait for k_clk_period*1;
 		  assert w_lights = "111111" report "bad flash" severity failure;
 		wait for k_clk_period*1;
-		  assert w_lights = "000000" report "bad flash" severity failure; 
-		  
-	    wait; 
-	   end process; 
-	
-	-----------------------------------------------------	
-	
+		  assert w_lights = "000000" report "bad flash" severity failure;
+ 
+		wait;
+	end process;
 end test_bench;
